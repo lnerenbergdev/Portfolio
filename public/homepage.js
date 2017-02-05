d36e980a07a7a6b3956f1d75d1b95bde6483f46b
Lawson Nerenberg <dartellis@gmail.com>
2017-02-05 10:15:51 -0700

    Updates

--- public/homepage.js
+++ public/homepage.js
@@ -1,42 +1,40 @@
 var planets = [];
 var vectors = [];
 
-var fieldSize = {x:70, y:35};
+var fieldSize = {x:20, y:20};
 
-var canvasSize = {x:1440, y:700};
-
-var text;
+var canvasSize = {x:0, y:0};
 
 function setup() {
-  createCanvas(canvasSize.x, canvasSize.y);
+  frameRate(30);
+  canvasSize = {x:displayWidth/3, y:windowHeight};
+  var canvas = createCanvas(canvasSize.x, canvasSize.y);
+  canvas.parent("canvas");
   
-
   background(0);
-  planets.push(new Body(250,250,50));
-  planets.push(new Body(250,350,25));
+  planets.push(new Body(int(displayWidth/3-(displayWidth/3/2)),int(windowHeight-200),50));
+  planets.push(new Body(int(displayWidth/3-(displayWidth/3/2)),int(windowHeight-200)+100,20));
   planets[1].velocity.x = 2;
   for(var i = 0; i < fieldSize.x; i++){
     vectors.push([]);
     for(var j = 0; j < fieldSize.y; j++){
-      vectors[i].push(new Body(i * int(canvasSize.x / fieldSize.x) + 25,j * int(canvasSize.y / fieldSize.y) + int((canvasSize.y / fieldSize.y)/2),5));
+      vectors[i].push(new Body(i * int(canvasSize.x / fieldSize.x),j * int(canvasSize.y / fieldSize.y) + int((canvasSize.y / fieldSize.y)/2),5));
     }
   }
 }
 
 function draw() {
-  background(20, 15, 0);
- 
-    planets[1].calculateAttraction(planets,1)
-    planets[1].update();
-    planets[1].display();
-    planets[0].display();
-  
+  background(250, 250, 250); 
   for(var i = 0; i < fieldSize.x; i++){
     for(var j = 0; j < fieldSize.y; j++){
       vectors[i][j].displayVector(planets,30);
       //vectors[i][j].display();
     }
   }
+  planets[1].calculateAttraction(planets,1)
+  planets[1].update();
+  planets[1].display();
+  planets[0].display();
 }
 
 
