6c7e6910809daf36c2c2a7476611c476cdf24be2
Lawson Nerenberg <dartellis@gmail.com>
2017-02-05 10:54:16 -0700

    fixed scailing

--- public/homepage.js
+++ public/homepage.js
@@ -3,22 +3,28 @@ var vectors = [];
 
 var fieldSize = {x:20, y:20};
 
-var canvasSize = {x:0, y:0};
+var canvasSize = {x:200, y:200};
 
 function setup() {
   frameRate(30);
+  var x_ = 0
+  if(windowWidth < 1000){
+    x_ = displayWidth;
+  } else{
+    x_ = displayWidth/3;
+  }
   canvasSize = {x:displayWidth/3, y:windowHeight};
   var canvas = createCanvas(canvasSize.x, canvasSize.y);
   canvas.parent("canvas");
   
   background(0);
-  planets.push(new Body(int(displayWidth/3-(displayWidth/3/2)),int(windowHeight-200),50));
-  planets.push(new Body(int(displayWidth/3-(displayWidth/3/2)),int(windowHeight-200)+100,20));
+  planets.push(new Body(int(displayWidth/3-(displayWidth/3/2)),int(windowHeight-175),50));
+  planets.push(new Body(int(displayWidth/3-(displayWidth/3/2)),int(windowHeight-175)+100,20));
   planets[1].velocity.x = 2;
   for(var i = 0; i < fieldSize.x; i++){
     vectors.push([]);
     for(var j = 0; j < fieldSize.y; j++){
-      vectors[i].push(new Body(i * int(canvasSize.x / fieldSize.x),j * int(canvasSize.y / fieldSize.y) + int((canvasSize.y / fieldSize.y)/2),5));
+      vectors[i].push(new Body(i * int(canvasSize.x / fieldSize.x) + 10,j * int(canvasSize.y / fieldSize.y) + int((canvasSize.y / fieldSize.y)/2),5));
     }
   }
 }
